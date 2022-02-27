78f773a8ae3702129e097407f4d18baf67f02757
shohan-pherones <shohan.sub.56@gmail.com>
2022-02-27 12:51:46 +0600

    class-2

---
+++ class-2/main.js
@@ -0,0 +1,19 @@
+//variable types
+//numeric
+var number = 100;
+var age = 25;
+
+//string
+var name = 'Nora Fatehi';
+var address = 'Kawak';
+var gf = 'Samantha';
+var universityName = 'Stamford university bangladesh';
+
+//boolean
+var pass = true;
+var subscribed = false;
+
+//exam
+var catQuantity = 1;
+var catName = 'Tom';
+var isCold = false;
