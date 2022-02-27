54284e352b5a0e174a3b160dfd7f8a0691e7fafe
shohan-pherones <shohan.sub.56@gmail.com>
2022-02-27 13:19:43 +0600

    class-3

---
+++ class-3/main.js
@@ -0,0 +1,19 @@
+//mathernatical operations
+var onionPrice = 40;
+var eggPrice = 33;
+var eggQuantity = 7;
+
+var totalPrice = onionPrice + eggPrice;
+console.log(totalPrice);
+
+var priceDifference = onionPrice - eggPrice;
+console.log(priceDifference);
+
+var totalEggPrice = eggPrice * eggQuantity;
+console.log(totalEggPrice);
+
+var moneyAvailable = 100;
+var orangePrice = 5;
+
+var orangeQuantity = moneyAvailable / orangePrice;
+console.log(orangeQuantity);
