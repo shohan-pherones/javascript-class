90eb4dace81288d3fcbf9c59a227b37c146e5cfa
shohan-pherones <shohan.sub.56@gmail.com>
2022-02-27 15:53:14 +0600

    class-4

---
+++ class-4/main.js
@@ -0,0 +1,40 @@
+//mathematical operation shorthand
+var price1 = 30;
+var price2 = 20;
+
+// var sum = price1 + price2;
+// var difference = price1 - price2;
+// var multiplication = price1 * price2;
+// var division = price1 / price2;
+
+// console.log(sum);
+// console.log(difference);
+// console.log(multiplication);
+// console.log(division);
+
+// var doublePrice = price1 * 2;
+// var newPrice = price1 + 10;
+// price1 = price1 + 10;
+
+price1 += 10;
+price2 -= 2;
+// console.log(price2);
+
+var age = 25;
+// age += 1;
+age++;
+// console.log(age);
+
+var love = 100;
+love--;
+// console.log(love);
+
+//exam
+var a = 10;
+var b = 20;
+// a = 20;
+// a+=10;
+a++;
+b--;
+var sum = a + b;
+console.log(sum);
